892296f330fa2f53cbb4f593488843e9a1c384d1
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-10-23 21:44:15 +0000

    Returns the list of school having a specific topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,12 @@
+#!/user/bin/env python3
+'''
+A schools_by_topic function module.
+'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''
+    Returns a list of documents with the specified topic
+    '''
+    match = mongo_collection.find({"topics": topic})
+    return match
