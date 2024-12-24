995b63c3b23cd62d00757fc940c8629d2f5c0498
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-12-24 11:41:23 +0000

    Task 10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,14 @@
+-- This script Creates a SafeDiv Function
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+    RETURN a / b;
+END $$
+
+DELIMITER ;
