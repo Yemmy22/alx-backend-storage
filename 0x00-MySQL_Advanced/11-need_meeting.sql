91f31cca3581301e4c6566c0ae245496ccee0f60
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-12-24 11:43:58 +0000

    Task 11

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,8 @@
+-- This Script Creates Need Meeting View
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80
+    AND (last_meeting IS NULL 
+        OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
